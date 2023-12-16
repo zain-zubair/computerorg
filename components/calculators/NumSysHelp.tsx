da22c1c6c61c7d800bf6f1444996b40015b9fc8b
Zain <zainzubair04@gmail.com>
2023-12-16 17:18:45 -0500

    Update NumSysHelp component style

--- components/calculators/NumSysHelp.tsx
+++ components/calculators/NumSysHelp.tsx
@@ -8,18 +8,18 @@ export default function NumSysHelp() {
     toggleHelp(!help);
   };
   return (
-    <section className="max-w-md mx-auto mt-8">
+    <section className="max-w-md mx-auto mt-8 p-10">
       <h2 className="text-sm text-center font-mono opacity-40 hover:opacity-80">
         <button onClick={handleToggle} className="underline">
           How To Use
         </button>
       </h2>
       {help && (
-        <div className="flex flex-col gap-4">
+        <div className="mt-4 flex flex-col gap-4">
           <div className="flex flex-col">
             <input
-              className="w-full px-3 py-1 font-mono text-sm bg-black outline-none border-b-2 border-white/25 hover:border-white/40 focus:border-white/80"
-              placeholder="eg. 1101)2"
+              className="w-full px-3 py-1 font-mono bg-Navy-light outline-none rounded-md hover:ring-1 focus:ring-2 ring-Slate transition-all duration-300"
+              placeholder="e.g. 1101)2"
             />
             <p className="mt-1 mb-5 text-sm">
               Enter a number followed by a &quot;)x&quot;, where x is indicating
@@ -30,7 +30,7 @@ export default function NumSysHelp() {
 
           <div className="flex flex-col">
             <input
-              className="text-sm px-3 py-1 font-mono bg-black outline-none border-b-2 border-white/25 hover:border-white/40 focus:border-white/80"
+              className="w-full px-3 py-1 font-mono bg-Navy-light outline-none rounded-md hover:ring-1 focus:ring-2 ring-Slate transition-all duration-300"
               placeholder="Base"
             />
             <p className="mt-1 mb-5 text-sm">
@@ -40,7 +40,7 @@ export default function NumSysHelp() {
           </div>
 
           <div className="flex flex-col">
-            <select className="w-full text-sm peer px-3 py-1 font-mono bg-black outline-none border-b-2 border-white/25 hover:border-white/40 focus:border-white/80">
+            <select className="w-full px-3 py-1 font-mono bg-Navy-light outline-none rounded-md hover:ring-1 focus:ring-2 ring-Slate transition-all duration-300">
               <option value="unsigned">Unsigned</option>
               <option value="1sComplement">1&apos;s Complement</option>
               <option value="2sComplement">2&apos;s Complement</option>
